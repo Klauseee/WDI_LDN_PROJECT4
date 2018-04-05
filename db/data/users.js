2ed6f474dd48e16dff05bfd71a15be6b513529bb
Sui <suii_shum@hotmail.com>
2018-04-05 10:27:26 +0100

    added users seed file

--- db/data/users.js
+++ db/data/users.js
@@ -1,18 +1,49 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  jobTitle: { type: String, required: 'Job title is required.' },
-  summary: { type: String, required: 'Include a summary of yourself.' },
-  yearsExp: { type: Number, required: 'Include the number of years you have been practising.' },
+module.exports = [{
+  jobTitle: 'Web Developer',
+  summary: 'I am a cool web developer',
+  yearsExp: 1,
   skills: {
-    frontend: [],
-    backend: []
+    frontend: ['Angular4', 'JavaScript', 'React'],
+    backend: ['Node.js', 'PHP'],
+    database: ['MongoDB']
   },
-  favouriteJobs: [{ type: mongoose.Schema.ObjectId, ref: 'Job' }],
-  matchedJobs: [{ type: mongoose.Schema.ObjectId, ref: 'Job' }],
-  cv: { type: String }
+  favouriteJobs: [],
+  matchedJobs: [],
+  cv: ''
 }, {
-  timestamps: true
-});
-
-module.exports = mongoose.model('User', userSchema);
+  jobTitle: 'Junior Web Developer',
+  summary: 'I am an awesome junior web developer',
+  yearsExp: 1,
+  skills: {
+    frontend: ['HTML', 'CSS', 'JavaScript'],
+    backend: ['Node.js', 'Ruby'],
+    database: ['NoSQL']
+  },
+  favouriteJobs: [],
+  matchedJobs: [],
+  cv: ''
+}, {
+  jobTitle: 'Full Stack Web Developer',
+  summary: 'I am a full stack web developer',
+  yearsExp: 4,
+  skills: {
+    frontend: ['Angular4', 'JavaScript', 'React'],
+    backend: ['C#', 'PHP'],
+    database: ['MySQL']
+  },
+  favouriteJobs: [],
+  matchedJobs: [],
+  cv: ''
+}, {
+  jobTitle: 'Software Engineer',
+  summary: 'I am a software engineer',
+  yearsExp: 6,
+  skills: {
+    frontend: ['Vue', 'JavaScript', 'React'],
+    backend: ['Java', 'C++'],
+    database: ['MongoDB']
+  },
+  favouriteJobs: [],
+  matchedJobs: [],
+  cv: ''
+}];
