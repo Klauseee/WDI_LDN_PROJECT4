9838b39f181ef84a5e86f80a542b0025ce3d6fb7
Nic Wilson <nicholaswilson3010@gmail.com>
2018-04-06 09:34:20 +0100

    Routing issue fixed

---
+++ src/lib/Technologies.js
@@ -0,0 +1,67 @@
+export default {
+  frontend: [{
+    name: 'javascript',
+    icon: 'devicon-javascript-plain colored'
+  }, {
+    name: 'html',
+    icon: 'devicon-html5-plain colored'
+  }, {
+    name: 'css',
+    icon: 'devicon-css3-plain colored'
+  }, {
+    name: 'sass',
+    icon: 'devicon-sass-original colored'
+  }, {
+    name: 'angularjs',
+    icon: 'devicon-angularjs-plain-wordmark colored'
+  }, {
+    name: 'jquery',
+    icon: 'devicon-jquery-plain colored'
+  }, {
+    name: 'krakenjs',
+    icon: 'devicon-krakenjs-plain colored'
+  }, {
+    name: 'laravel',
+    icon: 'devicon-laravel-plain colored'
+  }, {
+    name: 'less',
+    icon: 'devicon-less-plain-wordmark colored'
+  }, {
+    name: 'react',
+    icon: 'devicon-react-original-wordmark colored'
+  }, {
+    name: 'wordpress',
+    icon: 'devicon-wordpress-plain-wordmark colored'
+  }],
+  backend: [{
+    name: 'nodejs',
+    icon: 'devicon-nodejs-plain colored'
+  }, {
+    name: 'php',
+    icon: 'devicon-php-plain colored'
+  }, {
+    name: 'python',
+    icon: 'devicon-python-plain colored'
+  }, {
+    name: 'erlang',
+    icon: 'devicon-erlang-plain-wordmark colored'
+  }, {
+    name: 'express',
+    icon: 'devicon-express-original colored'
+  }, {
+    name: 'java',
+    icon: 'devicon-java-plain colored'
+  }, {
+    name: 'mysql',
+    icon: 'devicon-mysql-plain-wordmark colored'
+  }, {
+    name: 'rails',
+    icon: 'devicon-rails-plain-wordmark colored'
+  }, {
+    name: 'ruby',
+    icon: 'devicon-ruby-plain-wordmark colored'
+  }, {
+    name: 'mongodb',
+    icon: 'devicon-mongodb-plain-wordmark colored'
+  }]
+};
