a37261f94d689e1a9e444f70651c783c3c5f5a00
Nic Wilson <nicholaswilson3010@gmail.com>
2018-04-06 11:26:49 +0100

    Did some accidental work in dev

---
+++ src/components/users/UserShow.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import axios from 'axios';
+
+class UserShow extends React.Component {
+
+  state = {
+    user: null
+  }
+
+  componentDidMount() {
+    axios.get(`/api/users/${this.props.match.params.id}`)
+      .then(res => this.setState({ user: res.data }, () => this.state.user));
+  }
+
+  render() {
+    return(
+      <p>User Page</p>
+    );
+  }
+}
+
+export default UserShow;
