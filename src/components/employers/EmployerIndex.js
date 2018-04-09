d85c139ed140a20c8e1da7056413993448e5f415
Nic Wilson <nicholaswilson3010@gmail.com>
2018-04-09 16:40:17 +0100

    gestures removed from employers index

--- src/components/employers/EmployerIndex.js
+++ src/components/employers/EmployerIndex.js
@@ -2,7 +2,6 @@ import React from 'react';
 import axios from 'axios';
 // import _ from 'lodash';
 import Auth from '../../lib/Auth';
-import Hammer from 'react-hammerjs';
 
 import { Link } from 'react-router-dom';
 
@@ -16,23 +15,6 @@ class IndexRoute extends React.Component {
     axios.get('/api/employers')
       .then(res => this.setState({ employers: res.data, currentUser: Auth.getPayload().sub }));
   }
-
-  handleSwipeLeft = (e) => {
-    e.target.classList.add('slideOutLeft');
-    setTimeout(() => this.swipeRemove(e.target), 700);
-  }
-
-  handleSwipeRight = (e) => {
-    e.target.classList.add('slideOutRight');
-    setTimeout(() => this.swipeRemove(e.target), 700);
-
-  }
-
-  swipeRemove = (target) => {
-    target.parentNode.removeChild(target);
-    console.log('swipe occurred');
-  }
-
 
   // handleChange = (e) => {
   //   console.log(e.target.value);
@@ -64,27 +46,21 @@ class IndexRoute extends React.Component {
         </form> */}
         <ul className="columns is-mobile is-multiline">
           {this.state.employers.map((employer) =>
-            <Hammer
-              onSwipeLeft={this.handleSwipeLeft}
-              onSwipeRight={this.handleSwipeRight}
-              key={employer.name}
-              className="column is-one-third-desktop is-full-mobile animated">
-              <li>
-                <Link to={`/employers/${employer._id}`}>
-                  <div className="card">
-                    <div className="card-image">
-                      <figure className="image is-4by3">
-                        <img draggable="false" src={employer.logo} alt={`${employer.name} logo`} />
-                      </figure>
-                    </div>
-                    <div className="card-content">
-                      <h3 className="title is-4">{employer.name}</h3>
-                      <h4 className="subtitle">{employer.info}</h4>
-                    </div>
+            <li key={employer.name} className="column is-one-third-desktop is-full-mobile animated">
+              <Link to={`/employers/${employer._id}`}>
+                <div className="card">
+                  <div className="card-image">
+                    <figure className="image is-4by3">
+                      <img draggable="false" src={employer.logo} alt={`${employer.name} logo`} />
+                    </figure>
                   </div>
-                </Link>
-              </li>
-            </Hammer>
+                  <div className="card-content">
+                    <h3 className="title is-4">{employer.name}</h3>
+                    <h4 className="subtitle">{employer.info}</h4>
+                  </div>
+                </div>
+              </Link>
+            </li>
           )}
         </ul>
       </div>
