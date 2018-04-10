5c21da6282100fa676def5f3d56d4bdfb56969e6
Nic Wilson <nicholaswilson3010@gmail.com>
2018-04-10 15:16:32 +0100

    Trying to change matchedJobs in user model but it keeps repopulating because of virtual

---
+++ src/components/users/components/UserMatchedJobs.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+const UserMatchedJobs = ({ jobs, handleApply, handleDismiss }) => {
+  console.log(jobs);
+  return(
+    <div>
+      <h2 className="subtitle">Your matched jobs</h2>
+      {jobs && jobs.map(job =>
+        <div key={job.title} className="card">
+          <div className="card-content">
+            <h2 className="title">{job.title}</h2>
+            <h2 className="subtitle">{job.location}</h2>
+            <p>{job.summary}</p>
+            <h2 className="subtitle"><strong>Salary:</strong> £{job.salary}</h2>
+            <button className="button is-primary" onClick={() => handleApply(job._id)}>Accept</button>
+            <button className="button is-danger" onClick={() => handleDismiss(job._id)}>Dismiss</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UserMatchedJobs;
